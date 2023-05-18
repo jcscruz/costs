5c040c35b0a58ac21a538fefe01833dfc80723ef
julio.cruz <julio.cruz@g.globo>
2023-05-18 12:20:17 -0300

    aula 2

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # costs
 Curso React projeto Costs.  Gerenciamento de custos de projetos.
+
+# setup inicial do projeto
+    Criar o APP: npx create-react-app [nome do aplicativo]
+    Plugins auxiliares: npm install json-server react-router-dom uuid
+
+# Comandos
+    Rodar o projeto: npm start
--- costs/src/App.js
+++ costs/src/App.js
@@ -1,10 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './component/pages/Home'
+import Company from './component/pages/Company'
+import Contact from './component/pages/Contact'
+import NewProject from './component/pages/NewProject'
+
+import Container from './component/layout/Container'
+
 
 function App() {
   return (
     <div className="App">
-      <p>Costs</p>
+      <Router>
+        <div>
+            <Link to='/'>Home</Link>
+            <Link to='/company'>Empresa</Link>
+            <Link to='/contact'>Contato</Link>
+            <Link to='/newproject'>Novo Projeto</Link>
+        </div>
+        <Container customClass="min-height">
+          <Routes>
+            <Route exact path="/" element={<Home/>} />
+            <Route exact path="/company" element={<Company/>} />
+            <Route exact path="/contact" element={<Contact/>} />
+            <Route exact path="/newproject" element={<NewProject/>} />              
+          </Routes>
+        </Container>
+        <p>Footer</p>
+      </Router>
     </div>
   );
 }
